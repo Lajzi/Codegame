2ce9a6a7cf5244d6a9385a3c2b41c071e2ba4cca
Lajzi <lisa.hamberg@tretton37.com>
2018-05-12 12:45:57 +0200

    Changes structure in links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid, Row, Col } from "react-bootstrap";
+import { Grid, Row, Col, Nav, NavItem } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Header = () => (
@@ -8,16 +8,14 @@ const Header = () => (
       <Row>
         <Col md={12}>
           <h1>CODEIT</h1>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/rules">Rules</Link>
-              </li>
-            </ul>
-          </nav>
+          <Nav bsStyle="pills">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li role="presentation">
+              <Link to="/rules">Rules</Link>
+            </li>
+          </Nav>
         </Col>
       </Row>
     </Grid>
